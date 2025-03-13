71ea1ad8a73eb312decb1533f6c1605860c5372d
Joshua Reese <joshlreese@gmail.com>
2025-03-13 14:32:11 -0500

    Update the single cluster provider to engage the cluster when Run is called.

--- providers/single/provider.go
+++ providers/single/provider.go
@@ -45,7 +45,12 @@ func New(name string, cl cluster.Cluster) *Provider {
 }
 
 // Run starts the provider and blocks.
-func (p *Provider) Run(ctx context.Context, _ mcmanager.Manager) error {
+func (p *Provider) Run(ctx context.Context, mgr mcmanager.Manager) error {
+	if mgr != nil {
+		if err := mgr.Engage(ctx, p.name, p.cl); err != nil {
+			return err
+		}
+	}
 	<-ctx.Done()
 	return nil
 }
