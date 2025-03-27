180ecf0398debe6c544bfcb69c326375b0edd701
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2025-03-27 11:51:03 +0100

    Update hack/release.sh to new home
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -52,9 +52,9 @@ for DIR in providers examples; do
   for GOMOD in $(find "${DIR}" -name go.mod); do
     B=$(dirname ${GOMOD})
     if [ "$(uname)" = "Darwin" ]; then
-      stepi sed -i '' -E '/github\.com\/multicluster-runtime\/multicluster-runtime/ s/([[:space:]]+)v[[:alnum:]._+\-]+$/\1'"${VERSION}"'/' "${B}/go.mod"
+      stepi sed -i '' -E '/github\.com\/kubernetes-sigs\/multicluster-runtime/ s/([[:space:]]+)v[[:alnum:]._+\-]+$/\1'"${VERSION}"'/' "${B}/go.mod"
     else
-      stepi sed -i -E '/github\.com\/multicluster-runtime\/multicluster-runtime/ s/([[:space:]]+)v[[:alnum:]._+\-]+$/\1'"${VERSION}"'/' "${B}/go.mod"
+      stepi sed -i -E '/github\.com\/kubernetes-sigs\/multicluster-runtime/ s/([[:space:]]+)v[[:alnum:]._+\-]+$/\1'"${VERSION}"'/' "${B}/go.mod"
     fi
     step git diff "${B}/go.mod"
     echo
